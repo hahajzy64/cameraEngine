1fad41b6ac7b730bca6d1eaa78ab5ecd2d468735
hahajzy64 <hahajzy64@gmail.com>
2016-05-24 16:48:40 +0800

    test

--- cameraTestExpand/CameraEngine.m
+++ cameraTestExpand/CameraEngine.m
@@ -121,6 +121,7 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
     if (!_captureMovieFileOutput) {
         _captureMovieFileOutput = [[AVCaptureMovieFileOutput alloc]init];
     }
+    
     return _captureMovieFileOutput;
 }
 
